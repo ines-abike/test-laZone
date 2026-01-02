4ebb3f88cda1b79e769b3a9336bc4c2529b39d59
GitOmonwale <inesagbozo@gmail.com>
2026-01-02 23:28:09 +0100

    chore(config): delete unnecessary files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
-import { Provider } from "./provider";
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  display: "swap",
-})
+import { Provider } from "@/components/ui/provider"
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
   return (
-    <html>
-      <body className={roboto.className}>
+    <html suppressHydrationWarning>
+      <body>
         <Provider>{children}</Provider>
       </body>
     </html>
