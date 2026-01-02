1c19e0f5cfd9f6c251023a4a4dd69e7384e0bcef
GitOmonwale <inesagbozo@gmail.com>
2026-01-02 23:54:06 +0100

    fix(config): removed darkmode/lightmode logic from provider.tsx

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -3,5 +3,9 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider value={defaultSystem}>
+      {children}
+    </ChakraProvider>
+  );
 }
