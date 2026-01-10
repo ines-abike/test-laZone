b9566be2a505eb8e3882db04b3cada52aa7514c6
GitOmonwale <inesagbozo@gmail.com>
2026-01-10 17:31:17 +0100

    feat(theme): add color palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "./provider";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react";
 
 export default function Demo() {
   return (
-    <HStack>
-      <Button variant="solid" colorScheme="blue">Solid Button</Button>
-      <Button variant="outline" colorScheme="green">Outline Button</Button>
-    </HStack>
-  )
+    <div>
+      <Button variant="solid" bg="secondary.500">
+        Solid Button
+      </Button>
+      <Button bg="primary.500">
+        Outline Button
+      </Button>
+      <Box color="brand.title" fontSize="2xl">
+        Hello world
+      </Box>
+    </div>
+  );
 }
