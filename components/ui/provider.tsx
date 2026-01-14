56d7c0e9bfb8775323adf683f7f438277010526b
GitOmonwale <inesagbozo@gmail.com>
2026-01-14 23:58:53 +0100

    feat: add typography and button component

--- components/ui/provider.tsx
+++
@@ -1,9 +0,0 @@
-import { createSystem, defaultConfig, defineRecipe } from "@chakra-ui/react";
-
-const buttonRecipe = defineRecipe({});
-
-const system = createSystem(defaultConfig, {
-  theme: {
-    recipes: { button: buttonRecipe },
-  },
-});
