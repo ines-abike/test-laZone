5e87be9e829d6743bcec79bcabe95862b096b7e5
GitOmonwale <inesagbozo@gmail.com>
2026-01-17 04:04:59 +0100

    feat: correction of button, input and typography stories

--- components/Test.tsx
+++ components/Test.tsx
@@ -7,40 +7,15 @@ export default function Test() {
     <Stack p={8}>
       <Heading size="md">Buttons</Heading>
       <Stack direction="row">
-        <Button variant="solid" bg="secondary.500">
+        <Button variant="solid" colorPalette="primary">
           Solid Button
         </Button>
-        <Button variant="outline" borderColor="primary.500">
+        <Button variant="outline" colorPalette="primary">
           Outline Button
         </Button>
       </Stack>
 
       <Box textStyle="heading.huge">Headings</Box>
-
-      <Box textStyle="heading.huge">Heading Huge</Box>
-
-      <Box textStyle="heading.1">Heading 1</Box>
-      <Box textStyle="heading.1.sub">Subheading 1</Box>
-
-      <Box textStyle="heading.2">Heading 2</Box>
-      <Box textStyle="heading.2.sub">Subheading 2</Box>
-
-      <Box textStyle="heading.3">Heading 3</Box>
-      <Box textStyle="heading.3.sub">Subheading 3</Box>
-
-      <Box textStyle="heading.4">Heading 4</Box>
-      <Box textStyle="heading.4.sub">Subheading 4</Box>
-
-      <Heading size="md">Body</Heading>
-
-      <Box textStyle="body.default">Text Body</Box>
-      <Box textStyle="body.bold">Text Body Bold</Box>
-
-      <Box textStyle="body.small">Text Small</Box>
-      <Box textStyle="body.small.bold">Text Small Bold</Box>
-
-      <Box textStyle="body.micro">Text Micro</Box>
-      <Box textStyle="body.micro.bold">Text Micro Bold</Box>
     </Stack>
   );
 }
