48529c6ddddfa2602ff9813ff5afa050ed845e16
GitOmonwale <inesagbozo@gmail.com>
2026-01-17 04:04:59 +0100

    feat: correction of button, input and typography stories

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { Button } from "@chakra-ui/react";
 import Test from "@/components/Test";
 
 const Page = () => {
   return (
     <div>
-      <Button variant="ghost" colorPalette="secondary">
-        Solid Button
-      </Button>
       <Test />
     </div>
   );
