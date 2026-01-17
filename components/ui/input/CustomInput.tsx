941706f7ba531ccdbf851f70ee1a466e834e2ea2
GitOmonwale <inesagbozo@gmail.com>
2026-01-17 15:20:21 +0100

    fix: correct input story

---
+++ components/ui/input/CustomInput.tsx
@@ -0,0 +1,23 @@
+// components/CustomInput/CustomInput.tsx
+import { Input, Field } from "@chakra-ui/react";
+import type { InputProps } from "@chakra-ui/react";
+
+export interface CustomInputProps extends Omit<InputProps, "disabled"> {
+  placeholder?: string;
+  invalid?: boolean;
+  disabled?: boolean;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  invalid = false,
+  disabled = false,
+  ...inputProps
+}) => {
+  return (
+    <Field.Root invalid={invalid} disabled={disabled}>
+      <Input disabled={disabled} {...inputProps} />
+    </Field.Root>
+  );
+};
+
+export default CustomInput;
