dd3bc09cc3c978d6ba53ed9e7530d727b7eae5ac
GitOmonwale <inesagbozo@gmail.com>
2026-01-17 21:33:28 +0100

    fix: add export default on main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { Box, Button } from "@chakra-ui/react";
 
-const page = () => {
+const Demo = () => {
   return (
     <div>
       <Button variant="solid" bg="secondary.500">
         Solid Button
       </Button>
-      <Button bg="primary.500">Outline Button</Button>
+      <Button bg="error.500">Outline Button</Button>
       <Box color="brand.title" fontSize="2xl">
         Hello world
       </Box>
     </div>
   );
-}
+};
+
+export default Demo;
