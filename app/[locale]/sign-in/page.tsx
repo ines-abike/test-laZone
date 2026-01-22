6dbcaf85090d8b4da9b0b3fe1edb5d6d7ed338e8
GitOmonwale <inesagbozo@gmail.com>
2026-01-22 13:48:54 +0100

    feat: add en/fr app translation with next-intl

--- app/[locale]/sign-in/page.tsx
+++ app/[locale]/sign-in/page.tsx
@@ -9,45 +9,40 @@ import {
   Image,
   Text,
   Button,
+  Link,
 } from "@chakra-ui/react";
+<<<<<<< HEAD:app/sign-in/page.tsx
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
-  lastname: "",
-  firstname: "",
   email: "",
   password: "",
-  confirmPassword: "",
 };
 
 const onSubmit = () => {
-  alert("Votre compte a été créer avec succès !");
+  alert("Heureux de vous retrouver");
 };
 
 const validationSchema = Yup.object({
-  firstname: Yup.string()
-    .max(15, "Doit contenir au maximum 15 caractères")
-    .required("Champ obligatoire"),
-  lastname: Yup.string()
-    .max(20, "Doit contenir au maximum 20 caractères")
-    .required("Champ obligatoire"),
   email: Yup.string()
     .email("Adresse e-mail invalide")
     .required("Champ obligatoire"),
   password: Yup.string()
     .min(8, "Doit contenir au moins 8 caractères")
     .required("Champ obligatoire"),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password")], "Les mots de passe doivent correspondre")
-    .required("Champ obligatoire"),
 });
+=======
+import { useTranslations } from "next-intl";
+>>>>>>> 90dcb2b (feat: add en/fr app translation with next-intl):app/[locale]/sign-in/page.tsx
 
-const SignUp = () => {
+const SignIn = () => {
+  const t = useTranslations("auth.login");
+
   return (
     <Container maxW="full" p={0}>
       <Grid templateColumns={{ base: "1fr", lg: "2fr 1fr" }} minH="100vh">
-        <Flex align="center" justify="center" px="30px" py="80px">
+        <Flex align="center" justify="center" px="30px">
           <Box w="full" maxW="447px">
             <Flex direction="column" gap="24px">
               <Image
@@ -58,12 +53,13 @@ const SignUp = () => {
               />
 
               <Flex direction="column" gap="2px">
-                <Text textStyle="heading.3">Inscrivez-vous</Text>
+                <Text textStyle="heading.3">{t("title")}</Text>
                 <Text textStyle="text.small" color="gray.700">
-                  Créez votre compte
+                  {t("subtitle")}
                 </Text>
               </Flex>
 
+<<<<<<< HEAD:app/sign-in/page.tsx
               <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
@@ -71,49 +67,57 @@ const SignUp = () => {
               >
                 <Form>
                   <Flex direction="column" gap="16px">
-                    <Flex direction="row" gap="16px">
-                      <InputField
-                        name="lastname"
-                        type="text"
-                        label="Nom"
-                        placeholder="Doe"
-                      />
-
-                      <InputField
-                        name="firstname"
-                        type="text"
-                        label="Prénom"
-                        placeholder="John"
-                      />
-                    </Flex>
-
                     <InputField
-                      name="email"
-                      type="email"
                       label="Email"
+                      type="email"
                       placeholder="johndoe@mail.com"
+                      name="email"
                     />
 
                     <InputField
-                      name="password"
-                      type="password"
                       label="Mot de passe"
-                      placeholder="------"
-                      helperText="(8 caractères minimum)"
-                    />
-
-                    <InputField
-                      name="confirmPassword"
                       type="password"
-                      label="Répéter le mot de passe"
-                      placeholder="------"
+                      placeholder="Votre mot de passe"
+                      name="password"
                     />
+=======
+              <Box as="form">
+                <Flex direction="column" gap="16px">
+                  <Field.Root>
+                    <Field.Label textStyle="text.body">
+                      {t("email")}
+                    </Field.Label>
+                    <CustomInput type="email" placeholder="JohnDoe@mail.com" />
+                  </Field.Root>
+
+                  <Field.Root>
+                    <Field.Label textStyle="text.body">
+                      {t("password")}
+                    </Field.Label>
+                    <CustomInput type="password" placeholder="------" />
+>>>>>>> 90dcb2b (feat: add en/fr app translation with next-intl):app/[locale]/sign-in/page.tsx
+                    <Link
+                      href="/reset-password"
+                      textStyle="text.micro"
+                      color="gray.700"
+                      mt="4px"
+                      textDecorationStyle="unset"
+                    >
+                      {t("forgotPassword")}
+                    </Link>
+
+<<<<<<< HEAD:app/sign-in/page.tsx
                     <Button type="submit" bg="primary.900">
-                      S&apos;enregistrer
+                      Se connecter
                     </Button>
                   </Flex>
                 </Form>
               </Formik>
+=======
+                  <Button bg="primary.900">{t("submitButton")}</Button>
+                </Flex>
+              </Box>
+>>>>>>> 90dcb2b (feat: add en/fr app translation with next-intl):app/[locale]/sign-in/page.tsx
             </Flex>
           </Box>
         </Flex>
@@ -123,4 +127,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignIn;
