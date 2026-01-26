90e38675f0885564357920d0b87e64e59910636a
GitOmonwale <inesagbozo@gmail.com>
2026-01-26 20:49:41 +0100

    feat: add metadata on pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import siteConfig from "@/site-config";
 import { Provider } from "./provider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+};
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,100 +1,12 @@
-"use client";
-import InputField from "@/components/ui/input/InputField";
-import SideDecoration from "@/components/ui/SideDecoration";
-import {
-  Box,
-  Container,
-  Flex,
-  Grid,
-  Image,
-  Text,
-  Button,
-  Link,
-} from "@chakra-ui/react";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
+import { Metadata } from "next";
+import siteConfig from "@/site-config";
+import SignInForm from "@/components/SignInForm";
 
-const initialValues = {
-  email: "",
-  password: "",
+export const metadata: Metadata = {
+  title: siteConfig.auth.signIn.title,
+  description: siteConfig.auth.signIn.description,
 };
 
-const onSubmit = () => {
-  alert("Heureux de vous retrouver");
-};
-
-const validationSchema = Yup.object({
-  email: Yup.string()
-    .email("Adresse e-mail invalide")
-    .required("Champ obligatoire"),
-  password: Yup.string()
-    .min(8, "Doit contenir au moins 8 caractères")
-    .required("Champ obligatoire"),
-});
-
-const SignIn = () => {
-  return (
-    <Container maxW="full" p={0}>
-      <Grid templateColumns={{ base: "1fr", lg: "2fr 1fr" }} minH="100vh">
-        <Flex align="center" justify="center" px="30px">
-          <Box w="full" maxW="447px">
-            <Flex direction="column" gap="24px">
-              <Image
-                w={{ base: "200px", lg: "300px", md: "250px" }}
-                h={{ base: "40px", lg: "50px", md: "45px" }}
-                src="/logo.svg"
-                alt="logo"
-              />
-
-              <Flex direction="column" gap="2px">
-                <Text textStyle="heading.3">Connectez-vous</Text>
-                <Text textStyle="text.small" color="gray.700">
-                  Accédez à votre compte
-                </Text>
-              </Flex>
-
-              <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={onSubmit}
-              >
-                <Form>
-                  <Flex direction="column" gap="16px">
-                    <InputField
-                      label="Email"
-                      type="email"
-                      placeholder="johndoe@mail.com"
-                      name="email"
-                    />
-
-                    <InputField
-                      label="Mot de passe"
-                      type="password"
-                      placeholder="Votre mot de passe"
-                      name="password"
-                    />
-                    <Link
-                      href="/reset-password"
-                      textStyle="text.micro"
-                      color="gray.700"
-                      mt="4px"
-                    >
-                      Mot de passe oublié ?
-                    </Link>
-
-                    <Button type="submit" bg="primary.900">
-                      Se connecter
-                    </Button>
-                  </Flex>
-                </Form>
-              </Formik>
-            </Flex>
-          </Box>
-        </Flex>
-        <SideDecoration />
-      </Grid>
-    </Container>
-  );
-};
-
-export default SignIn;
+export default function SignInPage() {
+  return <SignInForm />;
+}
